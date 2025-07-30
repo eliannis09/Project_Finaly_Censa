f3a5bc584acec92d0ed515fe3b0fa498e41f6b2c
eliannis09 <eliannishez0904@gmail.com>
2025-07-29 21:04:59 -0500

    Nuevo cambio en el index

---
+++ Layout/Layout.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Esto e un viru manito</title>
+    <meta charset="utf-8">
+    <meta name="title" content="Titulo de la WEB"> 
+    <meta name="description" content="Descripcion de la WEB">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <script> src="script.js"</script>
+        <script type="text/javascrip">
+            /* Código */
+        </script>
+  </head>
--- index.php
+++ index.php
@@ -2,19 +2,8 @@
 //include_once "Controlador/registro.php"
 ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Esto e un viru manito</title>
-    <meta charset="utf-8">
-    <meta name="title" content="Titulo de la WEB"> 
-    <meta name="description" content="Descripcion de la WEB">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <script> src="script.js"</script>
-        <script type="text/javascrip">
-            /* Código */
-        </script>
-  </head>
+<?php include "Layout/Layout.php" ?>
+
   <body>
     <img src="images/firefox-icon.png" alt="Registro de pagina">
     <form class="row g-3" method="POST" action="Controlador/registro.php">
@@ -75,4 +64,112 @@
 				</div>
 			</form>
   </body>
+  <!-- Button trigger modal -->
+<div class = "col text-center">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  LISTAR REGISTRO 
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        
+      <table class="table table-dark table-striped-columns">
+	  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">Edad</th>
+      <th scope="col">Correo</th>
+      <th scope="col">Telefono</th>
+      <th scope="col">Editar</th>
+      <th scope="col">Eliminar</th>
+
+
+    </tr>
+  </thead>
+
+<?php
+include_once "Controlador/conexion.php";
+$conexion = new Conexion();
+$conexion = $conexion->conectar();
+if ($conexion){
+  $sql = "SELECT * FROM registropersonas";
+  $consulta = $conexion-> prepare($sql);
+  $consulta->execute();
+  $i = 0;
+  while($fila=$consulta -> fetch(PDO::FETCH_ASSOC )){
+   $i +=1; 
+  
+?>  
+
+  
+  <tbody>
+    <tr>
+      <th scope="row"><?php $i?></th>
+	 
+	  <td><?php echo $fila["Nombre"]; ?></td>
+	  <td><?php echo $fila["Apellido"]; ?></td>
+	  <td><?php echo $fila["Edad"]; ?></td>
+	  <td><?php echo $fila["Correo"]; ?></td>
+	  <td><?php echo $fila["Telefono"]; ?></td>
+	  <td><a href="Update.php" type="button" class="btn btn_success">Editar</a><td>
+	  <td></td>
+    
+     
+    </tr>
+    
+  </tbody>
+  <?php
+  }}
+  else {
+    echo"Error al conectar a la base de datos.";
+  }
+
+  ?>
+</table>
+      
+  </div>
+ 
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal genérico -->
+<div class="modal fade" id="mensajeModal" tabindex="-1" aria-labelledby="mensajeModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="mensajeModalLabel">Resultado</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+      </div>
+      <div class="modal-body" id="mensajeTexto">
+        <!-- Aquí va el mensaje dinámico -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+     <script src="js/modal.js"></script>
+
+
+
+    </body>
 </html>
